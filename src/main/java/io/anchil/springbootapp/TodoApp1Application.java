8426f58b301abe9457849771b62ba26fbbe2c701
xenonshinu89 <77114696+xenonshinu89@users.noreply.github.com>
2021-01-21 20:19:54 +0530

    todo-app commit

---
+++ src/main/java/io/anchil/springbootapp/TodoApp1Application.java
@@ -0,0 +1,17 @@
+package io.anchil.springbootapp;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApp1Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApp1Application.class, args);
+		
+	}
+
+}
